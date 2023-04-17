fd0fd3fe9799ead85e340b978cda50a9981a31da
Pedro Melo <pedromarcosprado26@gmail.com>
2023-04-17 00:53:25 -0300

    add cors()

--- src/Index.js
+++ src/Index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const rotas = require('./Rotas')
+const cors = require('cors');
 
 const app = express()
 
 app.use(express.json())
 
 app.use(rotas)
+app.use(cors())
 
 
 app.listen(3000)
