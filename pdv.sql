2b89cbd315e99a44554a7582509c7bbefc4af007
Pedro Melo <pedromarcosprado26@gmail.com>
2023-04-17 00:52:52 -0300

    espaços do texto do código

--- pdv.sql
+++ pdv.sql
@@ -1,18 +1,29 @@
 CREATE DATABASE PDV ;
 
-CREATE TABLE USUARIOS (
+CREATE TABLE USUARIOS
+(
   id serial primary key,
-	nome text,
-	email text unique,
-	senha text,
-  );
+  nome text,
+  email text unique,
+  senha text,
+);
 
-CREATE TABLE CATEGORIAS (
+CREATE TABLE CATEGORIAS
+(
   id serial primary key,
   descricao text
-	);
+);
 
-  
-INSERT INTO CATEGORIAS (descricao) 
+
+INSERT INTO CATEGORIAS
+  (descricao)
 values
-('Informática'),('Celulares'),('Beleza e Perfumaria'),('Mercado'),('Livros e Papelaria'),('Brinquedos'),('Moda'),('Bebê'),('Games')
+  ('Informática'),
+  ('Celulares'),
+  ('Beleza e Perfumaria'),
+  ('Mercado'),
+  ('Livros e Papelaria'),
+  ('Brinquedos'),
+  ('Moda'),
+  ('Bebê'),
+  ('Games')
