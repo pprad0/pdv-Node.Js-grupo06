f31dd214c63d218ff0d52faaac5cdf49e0af5bc0
Wdson Danrlei <44691537+wdson91@users.noreply.github.com>
2023-04-19 20:29:38 -0300

    Atualização de pastas

--- src/Index.js
+++ src/Index.js
@@ -4,9 +4,16 @@ const cors = require('cors');
 
 const app = express()
 
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+ app.use(cors(corsOptions))
 app.use(express.json())
 
 app.use(rotas)
-app.use(cors())
+
 
 app.listen(3000)
