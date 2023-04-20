ff2927db778989344d65c50449b5a25924180e2c
Pedro Melo <pedromarcosprado26@gmail.com>
2023-04-19 22:21:18 -0300

    Rota detalhar usuário

--- src/routes/Rotas.js
+++ src/routes/Rotas.js
@@ -4,7 +4,7 @@ const rotas = express();
 const listarCategories = require('../Controllers/CategoryControl');
 const autenticarUser = require('../Middlewares/UserAutentica');
 const loginUser = require('../Controllers/UserLogin');
-const cadastrarUser = require('../Controllers/UserControl');
+const { cadastrarUser, detalharUser } = require('../Controllers/UserControl');
 
 rotas.post('/usuario', cadastrarUser)
 rotas.get('/categorias', listarCategories)
@@ -12,5 +12,6 @@ rotas.post('/login', loginUser)
 
 rotas.use(autenticarUser)
 
+rotas.get('/usuario', detalharUser)
 
 module.exports = rotas;
