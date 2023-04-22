a2d99e74d17674fc5e830a1a8613467f70a6636c
Pedro Melo <pedromarcosprado26@gmail.com>
2023-04-22 12:05:27 -0300

    Rota editar usuário

--- src/Controllers/UserControl.js
+++ src/Controllers/UserControl.js
@@ -27,7 +27,6 @@ const cadastrarUser = async (req, res) => {
         return res.status(201).send()
 
     } catch (error) {
-        console.log(error)
         return res.status(500).json({ mensagem: 'O servidor apresentou um erro !' })
     }
 }
@@ -36,9 +35,40 @@ const detalharUser = async (req, res) => {
     res.status(200).json({ usuario: req.usuario })
 }
 
+const editarUser = async (req, res) => {
+    const { nome, email, senha } = req.body
 
+    if (!nome || !email || !senha) {
+        return res.status(400).json('A informação do nome, email e senha são parametros obrigatórios !')
+    }
+
+    try {
+
+        const existeEmail = await knex('usuarios').where({ email }).first()
+
+        if (existeEmail) {
+            return res.status(400).json('Email já cadastrado !')
+        }
+
+        const senhAcrip = await bcrypt.hash(senha, 10)
+
+        const usuario = await knex('usuarios')
+            .where('id', '=', req.usuario.id)
+            .update({
+                nome,
+                email,
+                senha: senhAcrip
+            })
+
+        return res.status(201).send()
+
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'O servidor apresentou um erro !' })
+    }
+}
 
 module.exports = {
     cadastrarUser,
-    detalharUser
+    detalharUser,
+    editarUser
 };
--- src/routes/Rotas.js
+++ src/routes/Rotas.js
@@ -4,7 +4,7 @@ const rotas = express();
 const listarCategories = require('../Controllers/CategoryControl');
 const autenticarUser = require('../Middlewares/UserAutentica');
 const loginUser = require('../Controllers/UserLogin');
-const { cadastrarUser, detalharUser } = require('../Controllers/UserControl');
+const { cadastrarUser, detalharUser, editarUser } = require('../Controllers/UserControl');
 
 rotas.post('/usuario', cadastrarUser)
 rotas.get('/categorias', listarCategories)
@@ -13,5 +13,6 @@ rotas.post('/login', loginUser)
 rotas.use(autenticarUser)
 
 rotas.get('/usuario', detalharUser)
+rotas.put('/usuario', editarUser)
 
 module.exports = rotas;
