73a23968a9085873e5d02add1d76da970391ae97
Pedro Melo <pedromarcosprado26@gmail.com>
2023-04-22 19:11:02 -0300

    Update Rotas.js

--- src/routes/Rotas.js
+++ src/routes/Rotas.js
@@ -6,15 +6,15 @@ const autenticarUser = require('../Middlewares/UserAutentica');
 const loginUser = require('../Controllers/UserLogin');
 const { cadastrarUser, detalharUser, editarUser } = require('../Controllers/UserControl');
 
-rotas.get('/',express.static("desafio-front"))
-rotas.get('/produtos',express.static("desafio-front"))
+rotas.get('/', express.static("desafio-front"))
+rotas.get('/produtos', express.static("desafio-front"))
 rotas.post('/usuario', cadastrarUser)
 rotas.get('/categorias', listarCategories)
 rotas.post('/login', loginUser)
 
-//rotas.use(autenticarUser)
+rotas.use(autenticarUser)
 
-rotas.get('/usuario', detalharUser,autenticarUser)
-rotas.put('/usuario', editarUser,autenticarUser)
+rotas.get('/usuario', detalharUser)
+rotas.put('/usuario', editarUser)
 
 module.exports = rotas;
