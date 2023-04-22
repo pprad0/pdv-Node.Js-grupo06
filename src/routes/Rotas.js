18b8738298f0d62810011da0fe24816b09b349fa
Wdson Danrlei <44691537+wdson91@users.noreply.github.com>
2023-04-22 17:29:53 -0300

    Update Rotas.js

--- src/routes/Rotas.js
+++ src/routes/Rotas.js
@@ -7,6 +7,7 @@ const loginUser = require('../Controllers/UserLogin');
 const { cadastrarUser, detalharUser, editarUser } = require('../Controllers/UserControl');
 
 rotas.get('/',express.static("desafio-front"))
+rotas.get('/produtos',express.static("desafio-front"))
 rotas.post('/usuario', cadastrarUser)
 rotas.get('/categorias', listarCategories)
 rotas.post('/login', loginUser)
