acef574590c1421a1f1aaf2040b5d58ca762f7e2
Wdson Danrlei <44691537+wdson91@users.noreply.github.com>
2023-05-01 10:10:42 -0300

    Update Index.js

--- src/Index.js
+++ src/Index.js
@@ -3,7 +3,7 @@ const rotas = require('./routes/Rotas')
 const cors = require('cors');
 
 const app = express()
-app.use(express.static("desafio-front"))
+//app.use(express.static("desafio-front"))
 const corsOptions = {
     origin: '*',
     credentials: true,            //access-control-allow-credentials:true
