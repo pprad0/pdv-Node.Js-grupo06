a4ad94bc7f331c2aec47bd168c121f64c26b1fcf
Pedro Melo <pedromarcosprado26@gmail.com>
2023-05-07 05:48:37 -0300

    Joi implementado, erros consertados, novas funções
    Funções para validar email e cpf. Cpf inválido não é mais inserido no banco de dados. Agora pode-se cadastrar cep, rua, numero, bairro etc. Retirado validação de token que estava dentro do arquivo.
    
    Sintaxe do update de cliente precisava de um 'where', que foi corrigido.

--- src/controllers/Clients.js
+++ src/controllers/Clients.js
@@ -1,87 +1,96 @@
 const knex = require('../db/Connection')
 
+
+const validarEmail = async (email) => {
+    const emailDuplicado = await knex('clientes').where("email", email).first();
+    const validacaoEmail = emailDuplicado ? true : false
+    return validacaoEmail
+}
+
+const validarCpf = async (cpf) => {
+    const cpfDuplicado = await knex('clientes').where("cpf", cpf).first();
+    const validacaoCpf = cpfDuplicado ? true : false
+    return validacaoCpf
+}
+
+
 const cadastrarCliente = async (req, res) => {
+    const { nome, email, cpf, cep, rua, numero, bairro, cidade, estado } = req.body;
 
-    const { nome, email, cpf } = req.body;
-
-    if(!nome || !email || !cpf){
-        return res.status(400).json({mensagem: 'A informação do nome, email e cpf são parametros obrigatórios !'});
+    if (cpf.includes('.') || cpf.length !== 11) {
+        return res.status(400).json({ mensagem: 'Insira um cpf válido e somente números, por favor.' })
     }
 
-    try {      
+    try {
+        if (await validarEmail(email) || await validarCpf(cpf)) {
+            return res.status(400).json({ mensagem: 'E-mail e/ou CPF já estão sendo utilizados!' });
+        };
 
-        const client = await knex('clientes').insert({
-        nome, 
-        email, 
-        cpf,
+        const cliente = await knex('clientes').insert({
+            nome,
+            email,
+            cpf,
+            cep: cep || null,
+            rua: rua || null,
+            numero: numero || null,
+            bairro: bairro || null,
+            cidade: cidade || null,
+            estado: estado || null
         })
-            
-        return res.status(201).send()
+            .returning('*')
+
+        return res.status(201).json({ mensagem: "Cliente cadastrado no sistema!", cliente: cliente })
 
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({mensagem: 'O servidor apresentou um erro !'});
+        return res.status(500).json({ mensagem: 'O servidor apresentou um erro !' });
     }
-
 }
 
 
 const editarCliente = async (req, res) => {
-    const { nome, email, cpf } = req.body;
-    const { authorization } = req.headers;
+    const { nome, email, cpf, cep, rua, numero, bairro, cidade, estado } = req.body;
+    const { id } = req.params
 
-    if (!nome, !email, !cpf) {
-        return res.status(400).json({ mensagem: 'É obrigatório o preenchimento de todos os campos!' })
-    };
-
-
+    if (cpf.includes('.') || cpf.length !== 11) {
+        return res.status(400).json({ mensagem: 'Insira um cpf válido e somente números, por favor.' })
+    }
     try {
-
-        const emailDuplicado = await knex('clientes').where("email", email);
-        const cpfDuplicado = await knex('cleintes').where("cpf", cpf);
-
-        if (emailDuplicado || cpfDuplicado) {
+        if (await validarEmail(email) || await validarCpf(cpf)) {
             return res.status(400).json({ mensagem: 'E-mail e/ou CPF já estão sendo utilizados!' });
         };
 
-
-        const token = authorization.split(' ')[1]
-        const { id } = await jwt.verify(token, senhaHash);
-
-        const validarUsuario = await knex('clientes').where("id", id);
-        if (!validarUsuario) {
-            return res.status(400).json({ mensagem: 'Não autorizado!' })
-        } else {
-            const editarCliente = await knex('clientes').update({
+        const editarCliente = await knex('clientes')
+            .where('id', id)
+            .update({
                 nome,
                 email,
-                cpf
-            });
-        };
+                cpf,
+                cep: cep || null,
+                rua: rua || null,
+                numero: numero || null,
+                bairro: bairro || null,
+                cidade: cidade || null,
+                estado: estado || null
+            })
+            .returning('*')
 
-        return res.status(201).json({ mensagem: 'Cliente atualizado com sucesso!' })
-
+        return res.status(201).json({ mensagem: 'Cliente atualizado com sucesso!', cliente: editarCliente })
     }
-    catch (erro) {
+    catch (error) {
         return res.status(500).json({ mensagem: 'O servidor apresentou um erro !' })
     };
-
-
-
 };
 
 
-const listarClientes = async (req,res) => {
+const listarClientes = async (req, res) => {
 
-    try{
+    try {
+        const listaDeClientes = await knex('clientes').returning('*');
+        return res.status(200).json(listaDeClientes);
 
-        clientesListar = await knex('clientes').returning('*');
-        return res.status(200).json(clientesListar);
-
-    }catch {
-        return res.status(500).json({ mensagem: 'O servidor apresentou um erro !'});
+    } catch {
+        return res.status(500).json({ mensagem: 'O servidor apresentou um erro !' });
     }
-
 }
 
 
@@ -89,10 +98,13 @@ const detalharCliente = async (req, res) => {
     const { id } = req.params;
 
     try {
-        const encontrarCliente = await knex('clientes').where("id", id);
+        const encontrarCliente = await knex('clientes').where("id", id).first();
 
-        return res.status(200).json(editarClientes.rows[0]);
+        if (!encontrarCliente) {
+            return res.status(400).json({ mensagem: "Cliente não encontrado, insira outro 'id' de cliente." })
+        }
 
+        return res.status(200).json(encontrarCliente);
 
     } catch (error) {
         return res.status(500).json({ mensagem: 'O servidor apresentou um erro !' })
@@ -104,5 +116,4 @@ module.exports = {
     editarCliente,
     listarClientes,
     detalharCliente
-
 };
