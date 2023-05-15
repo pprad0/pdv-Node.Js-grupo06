c062bc9c1f0184bf79eb28f8556a922449827934
Pedro Melo <pedromarcosprado26@gmail.com>
2023-05-15 20:52:35 -0300

    teste erro deploy

--- src/controllers/Category.js
+++ src/controllers/Category.js
@@ -1,9 +1,11 @@
 const knex = require('../db/Connection')
 
-const listarCategories = async(req, res) => {
+const listarCategories = async (req, res) => {
 
     try {
+        console.log("teste1")
         const categoriaListar = await knex('categorias').returning('*')
+        console.log("teste2")
         return res.status(200).json(categoriaListar)
 
     } catch (error) {
