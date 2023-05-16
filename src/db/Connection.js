bd5d881d0cf2f8004ad9894e0474699279d2dc0e
Pedro Melo <pedromarcosprado26@gmail.com>
2023-05-15 21:16:16 -0300

    url teste

--- src/db/Connection.js
+++ src/db/Connection.js
@@ -4,14 +4,16 @@ const knex = require('knex')({
 
     client: 'pg',
 
-    connection: {
-        host: process.env.HOST,
-        user: process.env.USER,
-        port: process.env.PORT,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
+    connection: 'postgres://kdimywow:6SAdrtRXF2tzPs8LNDa6Av5CjHyIq-rA@babar.db.elephantsql.com/kdimywow'
 
-    },
+    // host: process.env.HOST,
+    // user: process.env.USER,
+    // port: process.env.PORT,
+    // password: process.env.PASSWORD,
+    // database: process.env.DATABASE,
+
+
+
 
 });
 
